059dfb8b7a825c332c1ac29c9f94db25526875e1
abdielsouza <abdielcsouza@gmail.com>
2025-08-19 11:49:16 -0300

    added implementation for FixedString class.

--- src/container/fixed_string.cpp
+++ src/container/fixed_string.cpp
@@ -1,6 +1,48 @@
 #include "container.hpp"
 
-namespace auxutils
+namespace auxtypes
 {
-    
+    template <std::size_t N>
+    FixedString<N>::FixedString()
+    {
+        data[0] = '\0';
+    }
+
+    template <std::size_t N>
+    FixedString<N>::FixedString(const char* s)
+    {
+        assign(s);
+    }
+
+    template <std::size_t N>
+    void FixedString<N>::assign(const char* s)
+    {
+        length = std::min(strlen(s), N);
+        std::memcpy(data.data(), s, length);
+        data[length] = '\0';
+    }
+
+    template <std::size_t N>
+    const char* FixedString<N>::c_str() const
+    {
+        return data;
+    }
+
+    template <std::size_t N>
+    char& FixedString<N>::operator[](std::size_t i)
+    {
+        return data[i];
+    }
+
+    template <std::size_t N>
+    const char& FixedString<N>::operator[](std::size_t i) const
+    {
+        return data[i];
+    }
+
+    template <std::size_t N>
+    bool FixedString<N>::operator==(const char* s) const
+    {
+        return std::strcmp(data.data(), s) == 0;
+    }
 }
